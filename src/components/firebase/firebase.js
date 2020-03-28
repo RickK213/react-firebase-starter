f83763a5082e65c845fe94c09759c2cc4127366d
Rick Kippert <rkippert@att.net>
2020-03-28 02:40:23 -0500

    Add firebase authentication API

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,10 +1,32 @@
 import app from 'firebase/app';
 import { FIREBASE_CONFIG } from '../../../private/firebase-config';
+import 'firebase/auth';
 
 class Firebase {
   constructor() {
     app.initializeApp(FIREBASE_CONFIG);
+
+    this.auth = app.auth();
   }
+
+  // *** Auth API ***
+  // TODO: These endpoints are called asynchronously and need to be resolved
+  //       and handle errors
+  doCreateUserWithEmailAndPassword = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  doSignInWithEmailAndPassword = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  };
+
+  doSignOut = () => {
+    this.auth.signOut();
+  };
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
