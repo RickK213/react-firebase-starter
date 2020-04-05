4d5547446ad636c550be61ea58b336aa05101725
Rick Kippert <rkippert@att.net>
2020-04-04 23:42:17 -0500

    Write more tests for <SignUpForm />

--- src/components/screens/auth-screens/sign-up-screen/sign-up-form/sign-up-form.test.js
+++ src/components/screens/auth-screens/sign-up-screen/sign-up-form/sign-up-form.test.js
@@ -1,4 +1,4 @@
-/* global React */
+/* global React, shallow */
 import ShallowRenderer from 'react-test-renderer/shallow';
 import { createMemoryHistory } from 'history';
 import { SignUpFormComponent } from './sign-up-form';
@@ -11,5 +11,24 @@ describe('<SignUpFormComponent />', () => {
     renderer.render(<SignUpFormComponent history={history} />);
     const component = renderer.getRenderOutput();
     expect(component).toMatchSnapshot();
+  });
+
+  describe('this.handleOnChange', () => {
+    it('should return undefined if event is not set', () => {
+      const component = shallow(<SignUpFormComponent />);
+      const result = component.instance().handleOnChange();
+      expect(result).toEqual(undefined);
+    });
+
+    it('should call this.setState with the correct arguments if event is set', () => {
+      const event = {
+        target: { name: 'testName', value: 'testValue' }
+      };
+      const setState = jest.fn();
+      const component = shallow(<SignUpFormComponent />);
+      component.instance().setState = setState;
+      component.instance().handleOnChange(event);
+      expect(setState).toHaveBeenCalledWith({ testName: 'testValue' });
+    });
   });
 });
