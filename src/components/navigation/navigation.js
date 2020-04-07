1c9556ee6a38dad793d1a6fb382b73de3950667d
Rick Kippert <rkippert@att.net>
2020-04-06 23:24:08 -0500

    Display menu dependent on authUser state in <Navigation />

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { ROUTES } from '../../constants/routes';
 import { SignOutButton } from '../screens/auth-screens/sign-out-button/sign-out-button';
 
-export const Navigation = () => {
+export const Navigation = props => {
+  const { authUser } = props;
+
   return (
     <nav>
       <ul>
         <li>
           <Link to={ROUTES.HOME.path}>{ROUTES.HOME.label}</Link>
         </li>
-        <li>
-          <Link to={ROUTES.SIGN_UP.path}>{ROUTES.SIGN_UP.label}</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.SIGN_IN.path}>{ROUTES.SIGN_IN.label}</Link>
-        </li>
-        <li>
-          <Link to={ROUTES.ACCOUNT.path}>{ROUTES.ACCOUNT.label}</Link>
-        </li>
-        <li>
-          <SignOutButton />
-        </li>
+        {authUser ? (
+          <React.Fragment>
+            <li>
+              <Link to={ROUTES.ACCOUNT.path}>{ROUTES.ACCOUNT.label}</Link>
+            </li>
+            <li>
+              <SignOutButton />
+            </li>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <li>
+              <Link to={ROUTES.SIGN_UP.path}>{ROUTES.SIGN_UP.label}</Link>
+            </li>
+            <li>
+              <Link to={ROUTES.SIGN_IN.path}>{ROUTES.SIGN_IN.label}</Link>
+            </li>
+          </React.Fragment>
+        )}
       </ul>
     </nav>
   );
+};
+
+Navigation.propTypes = {
+  authUser: PropTypes.object
+};
+
+Navigation.defaultProps = {
+  authUser: null
 };
