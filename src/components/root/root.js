b29c9dd00f2c610a934e2dc085a359ee48689555
Rick Kippert <rkippert@att.net>
2020-04-06 23:23:42 -0500

    Remove <App /> and children from <Root />

--- src/components/root/root.js
+++ src/components/root/root.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from '../app/app';
-import { Navigation } from '../navigation/navigation';
-import { ROUTES } from '../../constants/routes';
-import { AccountScreen } from '../screens/account-screen/account-screen';
-import { HomeScreen } from '../screens/home-screen/home-screen';
 import Firebase, { FirebaseContext } from '../firebase';
-import { SignUpScreen } from '../screens/auth-screens/sign-up-screen/sign-up-screen';
-import { SignInScreen } from '../screens/auth-screens/sign-in-screen/sign-in-screen';
 
 export const Root = props => {
   const { store } = props;
@@ -16,25 +9,7 @@ export const Root = props => {
   return (
     <Provider store={store}>
       <FirebaseContext.Provider value={new Firebase()}>
-        <Router>
-          <App>
-            <Navigation />
-            <Switch>
-              <Route path={ROUTES.SIGN_UP.path}>
-                <SignUpScreen />
-              </Route>
-              <Route path={ROUTES.SIGN_IN.path}>
-                <SignInScreen />
-              </Route>
-              <Route path={ROUTES.ACCOUNT.path}>
-                <AccountScreen />
-              </Route>
-              <Route path={ROUTES.HOME.path}>
-                <HomeScreen />
-              </Route>
-            </Switch>
-          </App>
-        </Router>
+        <App />
       </FirebaseContext.Provider>
     </Provider>
   );
