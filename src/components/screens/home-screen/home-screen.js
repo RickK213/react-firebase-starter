78e6330993f1db7bc0f92c56c62d1d9219d846b8
Rick Kippert <rkippert@att.net>
2020-04-10 17:42:32 -0500

    Update <HomeScreen /> copy

--- src/components/screens/home-screen/home-screen.js
+++ src/components/screens/home-screen/home-screen.js
@@ -3,14 +3,14 @@ import { name, version } from '../../../../package.json';
 
 export const HomeScreen = () => {
   const greeting = `Welcome to ${name} `;
-  const VersionLabel = () => <code>{`(${version})`}</code>;
 
   return (
     <div>
-      <h2>
-        {greeting}
-        <VersionLabel />
-      </h2>
+      <code>
+        Version&nbsp;
+        {version}
+      </code>
+      <h2>{greeting}</h2>
       <h4>
         <span role="img" aria-label="Under Construction">
           ⚠️
