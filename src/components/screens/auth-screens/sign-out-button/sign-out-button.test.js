6315690a060023630df566af7143a2fb726bca5b
Rick Kippert <rkippert@att.net>
2020-04-10 22:54:01 -0500

    Add tests for <SignOutButton />

---
+++ src/components/screens/auth-screens/sign-out-button/sign-out-button.test.js
@@ -0,0 +1,24 @@
+/* global React, create, shallow */
+import { SignOutButtonComponent } from './sign-out-button';
+import { ROUTES } from '../../../../constants/routes';
+
+describe('<SignOutButtonComponent />', () => {
+  it('should render correctly', () => {
+    const component = create(<SignOutButtonComponent />).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  describe('this.handleOnSignOutClick', () => {
+    it('should call history.push if firebase call succeeds', async () => {
+      const push = jest.fn();
+      const history = { push };
+      const doSignOut = jest.fn().mockResolvedValue('default');
+      const firebase = { doSignOut };
+      const component = shallow(
+        <SignOutButtonComponent firebase={firebase} history={history} />
+      );
+      await component.instance().handleOnSignOutClick();
+      expect(push).toHaveBeenCalledWith(ROUTES.HOME.path);
+    });
+  });
+});
