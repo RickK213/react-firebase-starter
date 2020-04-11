6f7691c5c6903923e9df65c4f51cd1691ea106c7
Rick Kippert <rkippert@att.net>
2020-04-10 22:30:27 -0500

    Add tests for <AccountScreen />

--- src/components/screens/account-screen/account-screen.test.js
+++ src/components/screens/account-screen/account-screen.test.js
@@ -1,10 +1,14 @@
 /* global React, create */
+import { StaticRouter } from 'react-router-dom';
 import { AccountScreen } from './account-screen';
 
-// TODO: Fix this test. Need a <Router />
-describe.skip('<AccountScreen />', () => {
+describe('<AccountScreen />', () => {
   it('should render correctly', () => {
-    const component = create(<AccountScreen />).toJSON();
+    const component = create(
+      <StaticRouter context={{}}>
+        <AccountScreen />
+      </StaticRouter>
+    ).toJSON();
     expect(component).toMatchSnapshot();
   });
 });
